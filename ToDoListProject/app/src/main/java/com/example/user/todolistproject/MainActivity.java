9e21142e39e9361eb27df6e4c33c6a5ea8b60491
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-09-05 12:02:07 +0100

    added layout menu views

--- ToDoListProject/app/src/main/java/com/example/user/todolistproject/MainActivity.java
+++ ToDoListProject/app/src/main/java/com/example/user/todolistproject/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.user.todolistproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by user on 05/09/2016.
@@ -12,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                Log.d(TAG, "Add a new task");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
