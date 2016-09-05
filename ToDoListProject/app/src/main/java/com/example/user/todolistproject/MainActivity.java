e45787fbfe693f87a4bffe3aefb651a3e9fd6e3d
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-09-05 13:32:27 +0100

    sql add

--- ToDoListProject/app/src/main/java/com/example/user/todolistproject/MainActivity.java
+++ ToDoListProject/app/src/main/java/com/example/user/todolistproject/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.user.todolistproject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 /**
  * Created by user on 05/09/2016.
@@ -28,7 +31,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add_task:
-                Log.d(TAG, "Add a new task");
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                Log.d(TAG, "Task to add: " + task);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
                 return true;
 
             default:
---
+++ ToDoListProject/app/src/main/java/com/example/user/todolistproject/TaskContract.java
@@ -0,0 +1,20 @@
+package com.example.user.todolistproject;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by user on 05/09/2016.
+ */
+
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.user.todolistproject.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+        public static final String TASK_TITLE = "title";
+    }
+}
+    
+
