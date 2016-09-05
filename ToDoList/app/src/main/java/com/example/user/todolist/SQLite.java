5af1809f9a2330d4566d7fbde0a69a0b3241620a
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-09-05 11:57:10 +0100

    restarted

--- ToDoList/app/src/main/java/com/example/user/todolist/SQLite.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.todolist;
-
-/**
- * Created by user on 05/09/2016.
- */
-public class SQLite {
-}
---
+++ ToDoListProject/app/src/main/java/com/example/user/todolistproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.todolistproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 05/09/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
